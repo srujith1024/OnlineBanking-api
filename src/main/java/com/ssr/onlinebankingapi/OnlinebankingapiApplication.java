bdddab1b50f5438be20d73a26ea59bdad339a584
srujith <srujithr1024@gmail.com>
2018-05-03 23:47:00 -0500

    adding the project

---
+++ src/main/java/com/ssr/onlinebankingapi/OnlinebankingapiApplication.java
@@ -0,0 +1,12 @@
+package com.ssr.onlinebankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebankingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebankingapiApplication.class, args);
+	}
+}
